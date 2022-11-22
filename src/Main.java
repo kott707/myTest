ba5029dd85781eb28f8abb264ba42753e156eec1
kott707 <234782@mail.ru>
2022-11-22 16:20:35 +0500

    new

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println();
+        Putana putana = new Putana();
+        putana.getId();
     }
 }
---
+++ src/Putana.java
@@ -0,0 +1,12 @@
+public class Putana {
+
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
